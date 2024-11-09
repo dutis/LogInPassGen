977cf04fe21fb430f5316a5a74b8a96d39a9e7c6
dutis <m.m.rduch@gmail.com>
2024-11-09 13:19:01 +0100

    krok1

--- LoginGenerator.py
+++ LoginGenerator.py
@@ -1,2 +1,10 @@
 import secrets
 import string
+
+
+print("W tym programie wygenerujesz hasło podajac swoje imie i nazwisko:")
+
+
+imie = input("Podaj imie: ")
+nazwisko = input("Podaj nazwisko: ")
+
