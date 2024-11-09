cec704c7efc269c24083da616280cd1d61bada50
dutis <m.m.rduch@gmail.com>
2024-11-09 13:24:12 +0100

    Podstawka

--- LoginGenerator.py
+++ LoginGenerator.py
@@ -2,9 +2,29 @@ import secrets
 import string
 
 
-print("W tym programie wygenerujesz hasło podajac swoje imie i nazwisko:")
+print("W tym programie wygenerujesz hasło podajac swoje imie i nazwisko")
 
 
 imie = input("Podaj imie: ")
 nazwisko = input("Podaj nazwisko: ")
+
+
+def GenerujLogin(imie, nazwisko):
+    login = (imie[:3] + nazwisko[:3]).lower()
+    return login
+
+login = GenerujLogin(imie, nazwisko)
+
+
+def GenerujHaslo(length=12):
+    znaki = string.ascii_letters + string.digits + string.punctuation
+    haslo = ''.join(secrets.choice(znaki) for _ in range(length))
+    return haslo
+
+haslo = GenerujHaslo()
+
+
+print(login)
+print(haslo)
+
 
